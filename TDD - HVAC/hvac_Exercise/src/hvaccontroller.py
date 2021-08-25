0f93e65b02da4f134516dd7dc315087a981d0aed
Soeren Peters <peters@irmb.tu-bs.de>
2021-08-25 17:10:47 +0200

    Add empty files for tdd kata as starting point.

---
+++ TDD - HVAC/hvac_Exercise/src/hvaccontroller.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class HVAC(ABC):
+    """
+    An abstract class representing the HVAC hardware
+    """
+
+
+class HVACController:
+
+    def __init__(self) -> None:
---
+++ TDD - HVAC/hvac_Exercise/test/test_hvaccontroller.py
@@ -0,0 +1,5 @@
+import pytest
+
+from unittest.mock import MagicMock, patch
+from src.hvaccontroller import HVACController
+
