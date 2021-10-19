9309a4d1b3d41520527164dcb5a5af990ce2af75
Soeren Peters <peters.soeren@gmx.net>
2021-10-19 13:04:10 +0200

    New step 0 in inheritance with just a circle.

--- OOP/Inheritance/step_2.py
+++ OOP/Inheritance/step_2.py
@@ -1,92 +1,69 @@
-class Circle:
+from abc import ABC
 
-    def __init__(self, radius:float):
-        self._radius = radius
+
+class Shape(ABC):
+
+    def __init__(self) -> None:
         self._center_x = 0.
         self._center_y = 0.
-
+    
     def translate(self, delta_x: float, delta_y: float) -> None:
         self._center_x += delta_x
-        self._center_y -= delta_y
-
-    def __str__(self) -> str:
-        return f"Circle(center_x={self._center_x}, center_y={self._center_y}, radius={self._radius})"
+        self._center_y += delta_y
 
     def draw(self) -> None:
         print(self)
 
 
-class Rectangle:
+class Circle(Shape):
+
+    def __init__(self, radius:float):
+        super().__init__()
+        self._radius = radius
+        
+    def __str__(self) -> str:
+        return f"Circle(center_x={self._center_x}, center_y={self._center_y}, radius={self._radius})"
+
+
+class Rectangle(Shape):
 
     def __init__(self, width:float, height:float):
+        super().__init__()
         self._width = width
         self._height = height
-        self._center_x = 0.
-        self._center_y = 0.
-
-    def translate(self, delta_x: float, delta_y: float) -> None:
-        self._center_x += delta_x
-        self._center_y -= delta_y
 
     def __str__(self) -> str:
         return f"Rectangle(center_x={self._center_x}, center_y={self._center_y}, width={self._width}, height={self._height})"
 
-    def draw(self) -> None:
-        print(self)
 
-
-class Triangle:
+class Triangle(Shape):
 
     def __init__(self, length:float):
+        super().__init__()
         self._length = length
-        self._center_x = 0.
-        self._center_y = 0.
-
-    def translate(self, delta_x: float, delta_y: float) -> None:
-        self._center_x += delta_x
-        self._center_y -= delta_y
 
     def __str__(self) -> str:
         return f"Triangle(center_x={self._center_x}, center_y={self._center_y}, length={self._length})"
 
-    def draw(self) -> None:
-        print(self)
-
     
-class Hexagon:
+class Hexagon(Shape):
 
     def __init__(self, length:float):
+        super().__init__()
         self._length = length
-        self._center_x = 0.
-        self._center_y = 0.
-
-    def translate(self, delta_x: float, delta_y: float) -> None:
-        self._center_x += delta_x
-        self._center_y -= delta_y
 
     def __str__(self) -> str:
         return f"Hexagon(center_x={self._center_x}, center_y={self._center_y}, length={self._length})"
 
-    def draw(self) -> None:
-        print(self)
 
-
-class Square:
+class Square(Shape):
     
     def __init__(self, length:float):
+        super().__init__()
         self._length = length
-        self._center_x = 0.
-        self._center_y = 0.
-
-    def translate(self, delta_x: float, delta_y: float) -> None:
-        self._center_x += delta_x
-        self._center_y -= delta_y
 
     def __str__(self) -> str:
         return f"Square(center_x={self._center_x}, center_y={self._center_y}, length={self._length})"
-
-    def draw(self) -> None:
-        print(self)
 
 
 if __name__ == "__main__":
